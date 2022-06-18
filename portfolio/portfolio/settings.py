feac578e953a462f4a25626e191b2ab3c5d6da7e
LovroG05 <lovro.govekar@gmail.com>
2022-06-18 22:12:48 +0200

    if this works....

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "api.perchperkins.ml", "localhost", "127.0.0.1", "perchperkins.ml"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "perchperkins.ml"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
